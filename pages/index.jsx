fb6adab8bfdf7053305560e8918bd87ca6c7e8f6
Constance Osarodion Etiosa <54332180+Jonstance@users.noreply.github.com>
2023-08-02 15:59:44 +0100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -48,7 +48,7 @@ const Index2 = () => {
                   <img src="assets/images/hero/author3.png" alt="Author" />
                   <img src="assets/images/hero/author4.png" alt="Author" />
                   <i className="fal fa-plus" />
-                  <span className="text">100+ Popular Clients</span>
+                  <span className="text">0+ Popular Clients</span>
                 </div>
               </div>
             </div>
@@ -71,54 +71,6 @@ const Index2 = () => {
         </div>
       </section>
       {/* Hero Section End */}
-      {/* Advertise Area Start */}
-      <section className="advertise-banner-area rel z-1">
-        <div className="container container-1290">
-          <div className="row">
-            <div className="col-lg-7 wow fadeInUp delay-0-2s">
-              <div
-                className="advertise-banner style-one bgc-primary"
-                style={{
-                  backgroundImage:
-                    "url(assets/images/banner/add-banner-bg.png)",
-                }}
-              >
-                <div className="image">
-                  <img src="assets/images/banner/add-banner.png" alt="Banner" />
-                </div>
-                <div className="content mt-20">
-                  <span className="number">150+</span>
-                  <h6>Project Complete</h6>
-                  <hr />
-                  <p>Quis autem vel eum reprehe voluptate velit esse quam</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-5 wow fadeInUp delay-0-4s">
-              <div
-                className="advertise-banner style-two bg-white"
-                style={{
-                  backgroundImage: "url(assets/images/banner/star-vector.png)",
-                }}
-              >
-                <h3>Innovative Web Development Solutions</h3>
-                <hr className="mb-35" />
-                <div className="authors-text">
-                  <img src="assets/images/hero/author1.png" alt="Author" />
-                  <img src="assets/images/hero/author2.png" alt="Author" />
-                  <img src="assets/images/hero/author3.png" alt="Author" />
-                  <img src="assets/images/hero/author4.png" alt="Author" />
-                  <i className="fal fa-plus" />
-                  <span className="text">
-                    We denounce indignatione dislike mende charms
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* Advertise Area End */}
       
       {/* About Us Area start */}
       <section className="about-area pt-100 rpt-70 rel z-1">
@@ -127,7 +79,7 @@ const Index2 = () => {
             <div className="col-lg-7">
               <div className="about-content">
                 <div className="section-title mb-40 wow fadeInUp delay-0-2s">
-                  <span className="sub-title mb-15">About Company</span>
+                  <span className="sub-title mb-15">About Oyodigi</span>
                   <h2>We’re Award-winning Web Development Agency</h2>
                 </div>
                 <div className="text-left-border mt-60 mb-65 wow fadeInUp delay-0-2s">
