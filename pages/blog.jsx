be721ce4c791b2803b5108b7400bf192e5e31d14
Constance Osarodion Etiosa <54332180+Jonstance@users.noreply.github.com>
2023-08-23 13:33:32 +0100

    added Blog

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,7 +1,44 @@
 import PageBanner from "@/src/components/PageBanner";
 import Layout from "@/src/layout/Layout";
 import Link from "next/link";
-const Blog = () => {
+import { GraphQLClient } from 'graphql-request';
+
+const hygraph = new GraphQLClient(
+  'https://api-us-west-2.hygraph.com/v2/cllnfv0aq2c7801t5ghfnbhi3/master'
+);
+
+import { gql } from 'graphql-request';
+
+const QUERY = gql`
+  {
+    blogs {
+      createdAt
+      id
+      publishedAt
+      publishedOn
+      title
+      updatedAt
+      previewImage {
+        url
+      }
+    }
+    comments {
+      id
+    }
+  }
+`
+
+export async function getStaticProps() {
+  const { blogs } = await hygraph.request(QUERY)
+
+  return {
+    props: {
+      blogs
+    }
+  }
+}
+
+const Blog = ({blogs}) => {
   return (
     <Layout>
       {/* Page Banner Start */}
@@ -11,7 +48,8 @@ const Blog = () => {
       <section className="blog-page-area py-130 rpy-100 rel z-1">
         <div className="container container-1290">
           <div className="row">
-            <div className="col-xl-4 col-md-6">
+            {blogs.length > 0 && (
+              <div className="col-xl-4 col-md-6">
               <div className="blog-item wow fadeInUp delay-0-2s">
                 <div className="image">
                   <img src="assets/images/blog/blog1.jpg" alt="Blog" />
@@ -39,247 +77,11 @@ const Blog = () => {
                 </Link>
               </div>
             </div>
-            <div className="col-xl-4 col-md-6">
-              <div className="blog-item wow fadeInUp delay-0-4s">
-                <div className="image">
-                  <img src="assets/images/blog/blog2.jpg" alt="Blog" />
-                </div>
-                <ul className="blog-meta">
-                  <li>
-                    <i className="far fa-calendar-alt" />
-                    <a href="#">February 18, 2023</a>
-                  </li>
-                  <li>
-                    <i className="far fa-comments" />
-                    <a href="#">Comment (5)</a>
-                  </li>
-                </ul>
-                <hr />
-                <h4>
-                  <Link legacyBehavior href="/blog-details">
-                    <a>
-                      Inclusive Design And Accessibility Stream Heydon Pickering
-                    </a>
-                  </Link>
-                </h4>
-                <Link legacyBehavior href="/blog-details">
-                  <a className="read-more">
-                    Read More <i className="far fa-arrow-right" />
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6">
-              <div className="blog-item wow fadeInUp delay-0-6s">
-                <div className="image">
-                  <img src="assets/images/blog/blog3.jpg" alt="Blog" />
-                </div>
-                <ul className="blog-meta">
-                  <li>
-                    <i className="far fa-calendar-alt" />
-                    <a href="#">February 18, 2023</a>
-                  </li>
-                  <li>
-                    <i className="far fa-comments" />
-                    <a href="#">Comment (5)</a>
-                  </li>
-                </ul>
-                <hr />
-                <h4>
-                  <Link legacyBehavior href="/blog-details">
-                    <a>
-                      Creating Online Environments The Work Well For Older Users
-                    </a>
-                  </Link>
-                </h4>
-                <Link legacyBehavior href="/blog-details">
-                  <a className="read-more">
-                    Read More <i className="far fa-arrow-right" />
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6">
-              <div className="blog-item wow fadeInUp delay-0-2s">
-                <div className="image">
-                  <img src="assets/images/blog/blog4.jpg" alt="Blog" />
-                </div>
-                <ul className="blog-meta">
-                  <li>
-                    <i className="far fa-calendar-alt" />
-                    <a href="#">February 18, 2023</a>
-                  </li>
-                  <li>
-                    <i className="far fa-comments" />
-                    <a href="#">Comment (5)</a>
-                  </li>
-                </ul>
-                <hr />
-                <h4>
-                  <Link legacyBehavior href="/blog-details">
-                    <a>
-                      Tips Conducting Usability Studies With Participants
-                      Disabilities
-                    </a>
-                  </Link>
-                </h4>
-                <Link legacyBehavior href="/blog-details">
-                  <a className="read-more">
-                    Read More <i className="far fa-arrow-right" />
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6">
-              <div className="blog-item wow fadeInUp delay-0-4s">
-                <div className="image">
-                  <img src="assets/images/blog/blog5.jpg" alt="Blog" />
-                </div>
-                <ul className="blog-meta">
-                  <li>
-                    <i className="far fa-calendar-alt" />
-                    <a href="#">February 18, 2023</a>
-                  </li>
-                  <li>
-                    <i className="far fa-comments" />
-                    <a href="#">Comment (5)</a>
-                  </li>
-                </ul>
-                <hr />
-                <h4>
-                  <Link legacyBehavior href="/blog-details">
-                    <a>Online Environments That Work Well For Older Users</a>
-                  </Link>
-                </h4>
-                <Link legacyBehavior href="/blog-details">
-                  <a className="read-more">
-                    Read More <i className="far fa-arrow-right" />
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6">
-              <div className="blog-item wow fadeInUp delay-0-6s">
-                <div className="image">
-                  <img src="assets/images/blog/blog6.jpg" alt="Blog" />
-                </div>
-                <ul className="blog-meta">
-                  <li>
-                    <i className="far fa-calendar-alt" />
-                    <a href="#">February 18, 2023</a>
-                  </li>
-                  <li>
-                    <i className="far fa-comments" />
-                    <a href="#">Comment (5)</a>
-                  </li>
-                </ul>
-                <hr />
-                <h4>
-                  <Link legacyBehavior href="/blog-details">
-                    <a>
-                      Building Accessible Menu Systems Assistive Technology
-                      Users
-                    </a>
-                  </Link>
-                </h4>
-                <Link legacyBehavior href="/blog-details">
-                  <a className="read-more">
-                    Read More <i className="far fa-arrow-right" />
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6">
-              <div className="blog-item wow fadeInUp delay-0-2s">
-                <div className="image">
-                  <img src="assets/images/blog/blog7.jpg" alt="Blog" />
-                </div>
-                <ul className="blog-meta">
-                  <li>
-                    <i className="far fa-calendar-alt" />
-                    <a href="#">February 18, 2023</a>
-                  </li>
-                  <li>
-                    <i className="far fa-comments" />
-                    <a href="#">Comment (5)</a>
-                  </li>
-                </ul>
-                <hr />
-                <h4>
-                  <Link legacyBehavior href="/blog-details">
-                    <a>
-                      Inclusive Design And Accessibility Live Stream Heydon
-                      Pickering
-                    </a>
-                  </Link>
-                </h4>
-                <Link legacyBehavior href="/blog-details">
-                  <a className="read-more">
-                    Read More <i className="far fa-arrow-right" />
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6">
-              <div className="blog-item wow fadeInUp delay-0-4s">
-                <div className="image">
-                  <img src="assets/images/blog/blog8.jpg" alt="Blog" />
-                </div>
-                <ul className="blog-meta">
-                  <li>
-                    <i className="far fa-calendar-alt" />
-                    <a href="#">February 18, 2023</a>
-                  </li>
-                  <li>
-                    <i className="far fa-comments" />
-                    <a href="#">Comment (5)</a>
-                  </li>
-                </ul>
-                <hr />
-                <h4>
-                  <Link legacyBehavior href="/blog-details">
-                    <a>3d Cartoon Woman Sitting and Thinking to Solve Tasks</a>
-                  </Link>
-                </h4>
-                <Link legacyBehavior href="/blog-details">
-                  <a className="read-more">
-                    Read More <i className="far fa-arrow-right" />
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6">
-              <div className="blog-item wow fadeInUp delay-0-6s">
-                <div className="image">
-                  <img src="assets/images/blog/blog9.jpg" alt="Blog" />
-                </div>
-                <ul className="blog-meta">
-                  <li>
-                    <i className="far fa-calendar-alt" />
-                    <a href="#">February 18, 2023</a>
-                  </li>
-                  <li>
-                    <i className="far fa-comments" />
-                    <a href="#">Comment (5)</a>
-                  </li>
-                </ul>
-                <hr />
-                <h4>
-                  <Link legacyBehavior href="/blog-details">
-                    <a>
-                      Poster frame in living room mock up family member's hands
-                      holding
-                    </a>
-                  </Link>
-                </h4>
-                <Link legacyBehavior href="/blog-details">
-                  <a className="read-more">
-                    Read More <i className="far fa-arrow-right" />
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div className="col-lg-12">
+            )}
+            {!blogs.length > 0 && (<p className="text-black text-[20px] w-full text-center">No posts to show</p>)}
+
+            
+           {/*} <div className="col-lg-12">
               <ul className="pagination mt-10 flex-wrap justify-content-center wow fadeInUp delay-0-2s">
                 <li className="page-item disabled">
                   <span className="page-link">
@@ -316,7 +118,7 @@ const Blog = () => {
                   </a>
                 </li>
               </ul>
-            </div>
+            </div>*/}
           </div>
         </div>
       </section>
